dcda42783047387f717a674d24ef73e8b3149b31
franco <franco.labra@usach.cl>
2021-05-11 17:33:57 -0400

    cosas

--- lab2.r
+++ lab2.r
@@ -1,4 +1,5 @@
 require(randomForest)
+require(pROC)
 require(MASS)
 
 names <- c("id", "clumpThickness", "uniformityOfCellSize", "uniformityOfCellShape",
@@ -29,9 +30,18 @@ data <- na.omit(data)
 features <- data[,2:11]
 features$class <- factor(features$class, levels=c(2,4), labels=c("benigna","cancerosa"))
 
+data_set_size <- floor(nrow(features)/2)
+# Generate a random sample of "data_set_size" indexes
+indexes <- sample(1:nrow(features), size = data_set_size)
+# Assign the data to the correct sets
+training <- features[indexes,]
+validation1 <- features[-indexes,]
+
 ######### Random Forest ###########
 
+set.seed(20)
 features.rf <- randomForest(class ~ ., data=features, importance=TRUE, proximity=TRUE)
+
 print(features.rf)
 
 #Importancia: este nos permite aplicar el principio de parcimonia; regularizacion.; este nos permite ver que tanto afecta la variable en el proceso de clasificainon
@@ -56,14 +66,17 @@ par(op)
 print(features.mds$GOF)
 MDSplot(features.rf,features$class)
 
-features.rf100 <- randomForest(class ~ ., data=features,ntree =100, importance=TRUE, proximity=TRUE)
+set.seed(20)
+features.rf100 <- randomForest(class ~ ., data=features,ntree =100, mtry = 4, importance=TRUE, proximity=TRUE)
 print(features.rf100)
 
-features.rf600 <- randomForest(class ~ ., data=features,ntree =600, importance=TRUE, proximity=TRUE)
+set.seed(20)
+features.rf200 <- randomForest(class ~ ., data=features,ntree =200, mtry = 4, importance=TRUE, proximity=TRUE)
+print(features.rf200)
+
+set.seed(20)
+features.rf600 <- randomForest(class ~ ., data=features,ntree =600, mtry = 4, importance=TRUE, proximity=TRUE)
 print(features.rf600)
-
-features.rf100 <- randomForest(class ~ ., data=features,ntree =100, mtry=2, importance=TRUE, proximity=TRUE)
-print(features.rf100)
 
 features.rf100 <- randomForest(class ~ ., data=features,ntree =100, mtry=4, importance=TRUE, proximity=TRUE)
 print(features.rf100)
@@ -81,8 +94,52 @@ legend("bottomright",legend=c("cancerosa","benigna"),fill=2:4)
 
 features.mod1 <- features[,-9]
 
-features.rf100 <- randomForest(class ~ ., data=features.mod1, importance=TRUE, proximity=TRUE)
+set.seed(20)
+features.rf100 <- randomForest(class ~ ., data=features.mod1, ntree = 100, mtry = 4, importance=TRUE, proximity=TRUE)
 print(features.rf100)
+round(importance(features.rf100),2)
+varImpPlot(features.rf100)
+
+set.seed(20)
+features.rf200.2 <- randomForest(class ~ ., data=features.mod1, ntree = 200, mtry = 4, importance=TRUE, proximity=TRUE)
+print(features.rf200.2)
+
+set.seed(20)
+features.rf200.4 <- randomForest(class ~ ., data=features.mod1, ntree = 200, mtry = 4, importance=TRUE, proximity=TRUE)
+print(features.rf200.4)
+
+set.seed(20)
+features.rf600 <- randomForest(class ~ ., data=features.mod1, ntree = 600, mtry = 2, importance=TRUE, proximity=TRUE)
+print(features.rf600)
+round(importance(features.rf600),2)
+varImpPlot(features.rf600)
+
+### quitando adhesion con 100
+
+features.mod2 <- features.mod1[,-4]
+
+set.seed(20)
+features.rf100 <- randomForest(class ~ ., data=features.mod2, ntree = 100, mtry = 4, importance=TRUE, proximity=TRUE)
+print(features.rf100)
+round(importance(features.rf100),2)
+varImpPlot(features.rf100)
+
+### quitando normaNuclei y single con 100
+features.mod2 <- features.mod2[,-7]
+features.mod2 <- features.mod2[,-4]
+set.seed(20)
+features.rf100 <- randomForest(class ~ ., data=features.mod2, ntree = 100, mtry = 4, importance=TRUE, proximity=TRUE)
+print(features.rf100)
+
+
+#Importancia: este nos permite aplicar el principio de parcimonia; regularizacion.; este nos permite ver que tanto afecta la variable en el proceso de clasificainon
+round(importance(features.rf100),2)
+
+#MeanDecreaseAccuracy;  Que tanto incide que se encuentre o no la variable de clasificacion.
+#MeanCecreaseGini:      Tiene que ver con las impourezas de cada nodo al hacer las divicion de variables
+varImpPlot(features.rf100)
+
+
 
 features.rf100 <- randomForest(class ~ ., data=features.mod1,ntree =100, importance=TRUE, proximity=TRUE)
 print(features.rf100)
@@ -95,3 +152,48 @@ print(features.rf100)
 
 features.rf100 <- randomForest(class ~ ., data=features.mod1,ntree =100, mtry=4, importance=TRUE, proximity=TRUE)
 print(features.rf100)
+
+
+features.mod1 <- features[,-9]
+
+set.seed(20)
+features.rf100 <- randomForest(class ~ ., data=features.mod1, ntree = 100, mtry = 4, importance=TRUE, proximity=TRUE)
+print(features.rf100)
+round(importance(features.rf100),2)
+varImpPlot(features.rf100)
+
+
+library(ROCR)
+
+indexes <- sample(1:nrow(features), size = data_set_size)
+
+prediction_for_table <- predict(features.rf100,validation1[,-9])
+table(observed=validation1[,9],predicted=prediction_for_table)
+
+prediction_for_roc_curve <- predict(features.rf100,validation1[,-9],type="prob")
+
+# Use pretty colours:
+pretty_colours <- c("#F8766D","#00BA38")
+# Specify the different classes 
+classes <- levels(validation1$class)
+
+
+for (i in 1:2)
+{
+  # Define which observations belong to class[i]
+  true_values <- ifelse(validation1[,9]==classes[i],1,0)
+  # Assess the performance of classifier for class[i]
+  pred <- prediction(prediction_for_roc_curve[,i],true_values)
+  perf <- performance(pred, "tpr", "fpr")
+  if (i==1)
+  {
+    plot(perf,main="ROC Curve",col=pretty_colours[i]) 
+  }
+  else
+  {
+    plot(perf,main="ROC Curve",col=pretty_colours[i],add=TRUE) 
+  }
+  # Calculate the AUC and print it to screen
+  auc.perf <- performance(pred, measure = "auc")
+  print(auc.perf@y.values)
+}
